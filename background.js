d951bd0644c8e7ea2063c012ea512ee5f1aa1169
Laura S Adams <laurasadams@Lauras-MacBook-Pro-2.local>
2019-05-20 15:37:20 -0500

    onCreated and onRemoved are working (onUpdated needs work)

--- background.js
+++ background.js
@@ -10,62 +10,59 @@ function closedTab(){
 function newTab(){
     alert('new tab');
 }
-chrome.runtime.onMessage.addListener(function(response, sender, sendResponse){
-    alert(response);
-})
+// chrome.runtime.onMessage.addListener(function(response, sender, sendResponse){
+//     alert(response);
+// })
 
 
     //was getSelected, changed to tabs.query (working, just sends an 'alert' at least 4-5 times)
 
 chrome.tabs.onCreated.addListener(function(tabId, changeInfo, tab) {
-     //chrome.tabs.getSelected(null, function(tab) { 
+    //chrome.tabs.getSelected(null, function(tab) { 
     chrome.tabs.query({currentWindow: true, active: true}, function(tab){
         if(chrome.tabs.TabStatus = 'complete'){
-            var tabId = tab.id;
-            //var tabTitle = tab.title;
-            var tabUrl = tab.url;
+            var tabId = tab[0].id;
+            var tabUrl = tab[0].url;
             chrome.storage.sync.set({'a': tabId, 'b': tabUrl}, function(){
-                 alert('saved');
+                 //alert('saved');
              });
-            chrome.storage.sync.get('a', function(data){
+            chrome.storage.sync.get(['a', 'b'], function(data){
                 // chrome.downloads.download({
                 //     url: "data:text/plain;charset=utf-8," + encodeURIComponent("[InternetShortcut]\r\nURL=" + activeTab.url),
                 //     filename: '/Users/laurasadams/Downloads/TEUA',
                 //     saveAs: true
                 // });
-                alert(data.a + ': ' + tabUrl + ': on: ' + Date.now());
+                alert(data.a + ' : ' + data.b + ' : 1 : ' + Date.now());
             })
         }
     });
 });
 
+chrome.tabs.onRemoved.addListener(function(){
+    chrome.tabs.query({currentWindow: true, active: true}, function(tab){
+        var tabId = tab[0].id;
+        var tabUrl = tab[0].url;
+        chrome.storage.sync.set({'a': tabId, 'b': tabUrl}, function(){
+            //alert('saved');
+        });
+        chrome.storage.sync.get(['a', 'b'], function(data){
+            alert(data.a + ' : ' + data.b + ' : 0 : ' + Date.now());
+        })
+    })
+})
 
-
-// chrome.tabs.onRemoved.addListener(function(tabId, url, tabTitle, times){
-//     chrome.tabs.queryInfo({currentWindow: true, active: true}, function(tab){
-//         chrome.storage.sync.set({'a': tabId, 'b': tabUrl, 'c': Date.now}, function(){
-//             alert('saved');
-//         })
-//         chrome.storage.sync.get('c', function(data){
-//             alert(data.c);
-//         })
-//     });
-// });
-       
-
-
-// chrome.tabs.onUpdated.addListener(function(tabId, url, tab) {
-//     chrome.tabs.getSelected(null, function(tab) {
-//         var tabId = tab.id;
-//         var tabUrl = tab.url;
-//         chrome.storage.sync.set({'hi': tabUrl}, function(){
-//             alert('saved');
-//         });
-//         chrome.storage.sync.get('hi', function(data){
-//             alert(data.hi);
-//         })
-//     });
-// });
+chrome.tabs.onUpdated.addListener(function() {
+    chrome.tabs.query({currentWindow: true, active: true}, function(tab) {
+        var tabId = tab.id;
+        var tabUrl = tab.url;
+        chrome.storage.sync.set({'a': tabId, 'b': tabUrl}, function(){
+            alert('saved');
+        });
+        chrome.storage.sync.get('h', function(data){
+            alert(data.hi);
+        })
+    });
+});
 
 
 /*chrome.tabs.onCreated.addListener(function(tabId, changeInfo, tab) {
