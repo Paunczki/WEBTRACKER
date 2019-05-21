a55d9bf6b0f55b228c809bed105fe10633899d06
Laura S Adams <laurasadams@Lauras-MacBook-Pro-2.local>
2019-05-21 13:13:57 -0500

    timer still not working

--- background.js
+++ background.js
@@ -1,24 +1,5 @@
-//same thing for popup.js - https://stackoverflow.com/questions/34957319/how-to-listen-for-url-change-with-chrome-extension
-function changeURL(){
-    alert('url change');
-}
-
-function closedTab(){
-   alert('tab closed');
-}
-
-function newTab(){
-    alert('new tab');
-}
-// chrome.runtime.onMessage.addListener(function(response, sender, sendResponse){
-//     alert(response);
-// })
-
-
-    //was getSelected, changed to tabs.query (working, just sends an 'alert' at least 4-5 times)
-
+//working onCreated
 chrome.tabs.onCreated.addListener(function(tabId, changeInfo, tab) {
-    //chrome.tabs.getSelected(null, function(tab) { 
     chrome.tabs.query({currentWindow: true, active: true}, function(tab){
         if(chrome.tabs.TabStatus = 'complete'){
             var tabId = tab[0].id;
@@ -38,6 +19,7 @@ chrome.tabs.onCreated.addListener(function(tabId, changeInfo, tab) {
     });
 });
 
+//working onRemoved
 chrome.tabs.onRemoved.addListener(function(){
     chrome.tabs.query({currentWindow: true, active: true}, function(tab){
         var tabId = tab[0].id;
@@ -51,24 +33,117 @@ chrome.tabs.onRemoved.addListener(function(){
     })
 })
 
-// chrome.tabs.onUpdated.addListener(function() {
-//     chrome.tabs.query({currentWindow: true, active: true}, function(tab) {
-//         var tabId = tab.id;
-//         var tabUrl = tab.url;
-//         chrome.storage.sync.set({'a': tabId, 'b': tabUrl}, function(){
-//             alert('saved');
-//         });
-//         chrome.storage.sync.get('h', function(data){
-//             alert(data.hi);
-//         })
+// onUpdated (in progress)
+chrome.tabs.onUpdated.addListener(function() {
+    chrome.tabs.query({currentWindow: true, active: true}, function(tab) {
+        var tabId = tab[0].id;
+        var tabUrl = tab[0].url;
+        chrome.storage.sync.set({'a': tabId, 'b': tabUrl}, function(){
+            //alert('saved');
+        })
+        chrome.storage.sync.get(['a', 'b'], function(data){
+            alert(data.a + ' : ' + data.b + ' : 0 : ' + Date.now());
+        })
+    });
+});
+
+//new function dedicated to onCreated for onUpdated
+function created(){
+    chrome.tabs.onCreated.addListener(function(tabId, changeInfo, tab) {
+        chrome.tabs.query({currentWindow: true, active: true}, function(tab){
+            if(chrome.tabs.TabStatus = 'complete'){
+                var tabId = tab[0].id;
+                var tabUrl = tab[0].url;
+                chrome.storage.sync.set({'a': tabId, 'b': tabUrl}, function(){
+                     //alert('saved');
+                 });
+                chrome.storage.sync.get(['a', 'b'], function(data){
+                    // chrome.downloads.download({
+                    //     url: "data:text/plain;charset=utf-8," + encodeURIComponent("[InternetShortcut]\r\nURL=" + activeTab.url),
+                    //     filename: '/Users/laurasadams/Downloads/TEUA',
+                    //     saveAs: true
+                    // });
+                    alert(data.a + ' : ' + data.b + ' : 1 : ' + Date.now());
+                });
+            }
+        });
+    });
+}
+
+//new function dedicated to onRemoved for onUpdated
+function removed(){
+    chrome.tabs.onRemoved.addListener(function(){
+        chrome.tabs.query({currentWindow: true, active: true}, function(tab){
+            var tabId = tab[0].id;
+            var tabUrl = tab[0].url;
+            chrome.storage.sync.set({'a': tabId, 'b': tabUrl}, function(){
+                //alert('saved');
+            });
+            chrome.storage.sync.get(['a', 'b'], function(data){
+                alert(data.a + ' : ' + data.b + ' : 0 : ' + Date.now());
+            });
+        });
+    });
+}
+
+
+
+
+
+
+
+// function Update(t, tabId, url) {
+//     if (!url) {
+//       return;
+//     }
+//     if (tabId in History) {
+//       if (url == History[tabId][0][1]) {
+//         return;
+//       }
+//     } else {
+//       History[tabId] = [];
+//     }
+//     History[tabId].unshift([t, url]);
+  
+//     var history_limit = parseInt(localStorage["history_size"]);
+//     if (! history_limit) {
+//       history_limit = 23;
+//     }
+//     while (History[tabId].length > history_limit) {
+//       History[tabId].pop();
+//     }
+  
+//     chrome.browserAction.setBadgeText({ 'tabId': tabId, 'text': '0:00'});
+//     chrome.browserAction.setPopup({ 'tabId': tabId, 'popup': "popup.html#tabId=" + tabId});
+//   }
+  
+//   function HandleUpdate(tabId, changeInfo, tab) {
+//     Update(new Date(), tabId, changeInfo.url);
+//   }
+  
+//   function HandleRemove(tabId, removeInfo) {
+//     delete History[tabId];
+//   }
+  
+//   function HandleReplace(addedTabId, removedTabId) {
+//     var t = new Date();
+//     delete History[removedTabId];
+//     chrome.tabs.get(addedTabId, function(tab) {
+//       Update(t, addedTabId, tab.url);
 //     });
-// });
-
-
-/*chrome.tabs.onCreated.addListener(function(tabId, changeInfo, tab) {
-    alert('new tab');
-    chrome.tabs.onRemoved.addEventListener('hashchange', changeURL, false)
-});*/
-
-    //for onCreated
-//chrome.tabs.query({currentWindow: true, active : true}, funtion(tab) 
+//   }
+  
+  
+//   function UpdateBadges() {
+//     var now = new Date();
+//     for (tabId in History) {
+//       var description = FormatDuration(now - History[tabId][0][0]);
+//       chrome.browserAction.setBadgeText({ 'tabId': parseInt(tabId), 'text': description});
+//     }
+//   }
+  
+//   setInterval(UpdateBadges, 1000);
+  
+//   chrome.tabs.onUpdated.addListener(HandleUpdate);
+//   chrome.tabs.onRemoved.addListener(HandleRemove);
+//   chrome.tabs.onReplaced.addListener(HandleReplace);
--- popup.js
+++ popup.js
@@ -4,50 +4,36 @@
     })
 })*/
 
-function myAlert(){
-    alert('this is a web tracker')
-}
-document.addEventListener('DOMContentLoaded', function(){
-    document.getElementById('alertButton').addEventListener('click', myAlert);
-})
+// function myAlert(){
+//     alert('this is a web tracker')
+// }
+// document.addEventListener('DOMContentLoaded', function(){
+//     document.getElementById('alertButton').addEventListener('click', myAlert);
+// })
 
-chrome.runtime.sendMessage(document.getElementsByTagName('title')[0].innerHTML);
+// chrome.runtime.sendMessage(document.getElementsByTagName('title')[0].innerHTML);
 
-//changing button text when clicked
-function change() // no ';' here
-{
-    var elem = document.getElementById("button");
-    if (elem.value == "Off") elem.value = "On";
-    else elem.value = "Off";
-}
+// //changing button text when clicked
+// function change() // no ';' here
+// {
+//     var elem = document.getElementById("button");
+//     if (elem.value == "Off") elem.value = "On";
+//     else elem.value = "Off";
+// }
 
-
-
-
-
-
-
-
-
-
-
-
-
-//not sure which will work
-//these 2 are together
-/*
-chrome.tabs.onCreated.addEventListener((tabId, createdInfo) => {  
-    
+$("#button1").click(function(){
+    var counter = 1;
+    alert('ye');
+    setInterval(function() {
+        counter--;
+        if(counter >= 0){
+            span = document.getElementById("count");
+            span.innerHTMl = counter;
+            alert('ye');
+        }
+        if(counter === 0) {
+            alert('sorry, out of time');
+            clearInterval(counter);
+        }
+    }, 1000);
 });
-
-chrome.window.onCreated.addListener((tabID, createdInfo)) 
-
-chrome.tabs.onRemoved.addEventListener((tabId, removeInfo) => {
-    console.log(`The tab with id: ${tabId}, is closing`);
-});
-
-//this one is alone but is on background.js as well - https://stackoverflow.com/questions/34957319/how-to-listen-for-url-change-with-chrome-extension
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    alert('updated from contentscript');
- });
- */
