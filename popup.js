ff8a62592fde8680de2632fb68763ab83b07bc27
Laura Adams <50840905+lauraadams2@users.noreply.github.com>
2019-05-22 16:06:40 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,50 +1,14 @@
-/*$(function(){
-    $('click').keyup(function(){
-        $('#greet').text(Date.now() + ' ' + $('#name').val());
-    })
-})*/
-
-// function myAlert(){
-//     alert('this is a web tracker')
-// }
-// document.addEventListener('DOMContentLoaded', function(){
-//     document.getElementById('alertButton').addEventListener('click', myAlert);
-// })
-
-// chrome.runtime.sendMessage(document.getElementsByTagName('title')[0].innerHTML);
-
-// //changing button text when clicked
-// function change() // no ';' here
-// {
-//     var elem = document.getElementById("button");
-//     if (elem.value == "Off") elem.value = "On";
-//     else elem.value = "Off";
-// }
-
-// $("#button1").click(function(){
-//     var counter = 10;
-//     alert('ye');
-//     setInterval(function() {
-//         counter--;
-//         if(counter >= 0){
-//             span = document.getElementById("count");
-//             span.innerHTMl = counter;
-//             alert('ye');
-//         }
-//         if(counter === 0) {
-//             alert('sorry, out of time');
-//             clearInterval(counter);
-//         }
-//     }, 1000);
-// });
-
-
-window.onload=function()
-  {
-   document.getElementById("button1").onclick=function()
-    {
-  var websiteVar = document.getElementById('userWebsite').value;
-  chrome.runtime.sendMessage({ website: websiteVar, message:"Go_To_Clicked"});
-}
-  }
-
+//working on/off change
+$(function() {
+    var switchStatus = false;
+    $("#togBtn").on('change', function() {
+        if($('#box').is(':checked')) {
+            switchStatus = $("#togBtn").is(':checked');
+            alert(switchStatus); //to verify
+        }
+        else {
+            switchStatus = $("#togBtn").is(':checked');
+            alert(switchStatus); //to verify
+        }
+    });
+});
