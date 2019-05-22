f1de98574d4fd5eb6c9824fb731fdb63c97d2deb
Paunczki <johnnymikos@gmail.com>
2019-05-22 14:08:21 -0500

    Need to now find a way to check if the basic part of URL is the same

--- background.js
+++ background.js
@@ -1,12 +1,18 @@
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
-    if(changeInfo.status === 'loading'){
-        // var site = chrome.storage.sync.get(tabID);
-        // alert(site);
-        
-        alert(tabId + "  -  " + tab.url);
-
+    if(changeInfo.status === 'loading') {
+        var bs = tab.url;
+        chrome.storage.sync.get(['tabID'], function(data){
+            if(!(data.tabID === bs)){
+                alert(tabId + "  -  " + data.tabID + "  -  0  -  " + Date.now());
+                chrome.storage.sync.set({'tabID': bs}, function(){
+                    alert(tabId + "  -  " + bs + "  -  1  -  " + Date.now());
+                });
+            } 
+        });
     }
 });
 chrome.tabs.onRemoved.addListener(function(tabId, removeInfo){
-    alert(tabId);
+    chrome.storage.sync.get(['tabID'], function(data){
+        alert(tabId + "  -  " + data.tabID + "  -  0  -  " + Date.now());
+    });
 });
