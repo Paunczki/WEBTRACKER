0050c69a5722d9f0aee354c4c020360e055ad59f
Laura Adams <laura.adams2233@gmail.com>
2019-05-24 11:24:50 -0500

    popup.js is working with the chrome.storage.local.get from the set in background.js. However, the checkbox is still not saving across tabs

--- background.js
+++ background.js
@@ -1,26 +1,56 @@
-chrome.extension.onConnect.addListener(function(port) {
-    console.log("Connected .....");
-        port.onMessage.addListener(function(msg) {
-         console.log("message recieved" + msg);
-         port.postMessage(" Hi Popup.js");
-    });
-})
+// chrome.extension.onConnect.addListener(function(port) {
+//     console.log("Connected .....");
+//         port.onMessage.addListener(function(msg) {
+//          console.log("message recieved" + msg);
+//          port.postMessage(" Hi Popup.js");
+//     });
+// })
 
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
-    if(changeInfo.status === 'loading') {
-        var bs = tab.url;
-        chrome.storage.sync.get(['tabID'], function(data){
-            if(!(data.tabID === bs)){
-                alert(tabId + "  -  " + data.tabID + "  -  0  -  " + Date.now());
-                chrome.storage.sync.set({'tabID': bs}, function(){
-                    alert(tabId + "  -  " + bs + "  -  1  -  " + Date.now());
+//var switchStatus = false;
+chrome.storage.local.set({'switchStatus': false});
+
+//not working because the checkbox isnt saving across all tabs or even when closed. 
+chrome.storage.onChanged.addListener(function(){
+    while(chrome.storage.local.get('switchStatus') == true) {
+        chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
+            if(changeInfo.status === 'loading') {
+                var bs = tab.url;
+                chrome.storage.sync.get(['tabID'], function(data){
+                    if(!(data.tabID === bs)){
+                        alert(tabId + "  -  " + data.tabID + "  -  0  -  " + Date.now());
+                        chrome.storage.sync.set({'tabID': bs}, function(){
+                            alert(tabId + "  -  " + bs + "  -  1  -  " + Date.now());
+                        });
+                    } 
                 });
-            } 
+            }
         });
-    }
+
+        chrome.tabs.onRemoved.addListener(function(tabId, removeInfo){
+            chrome.storage.sync.get(['tabID'], function(data){
+                alert(tabId + "  -  " + data.tabID + "  -  0  -  " + Date.now());
+            });
+        });        
+    }   
 });
-chrome.tabs.onRemoved.addListener(function(tabId, removeInfo){
-    chrome.storage.sync.get(['tabID'], function(data){
-        alert(tabId + "  -  " + data.tabID + "  -  0  -  " + Date.now());
-    });
-});
+
+// chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
+//     if(changeInfo.status === 'loading') {
+//         var bs = tab.url;
+//         chrome.storage.sync.get(['tabID'], function(data){
+//             if(!(data.tabID === bs)){
+//                 alert(tabId + "  -  " + data.tabID + "  -  0  -  " + Date.now());
+//                 chrome.storage.sync.set({'tabID': bs}, function(){
+//                     alert(tabId + "  -  " + bs + "  -  1  -  " + Date.now());
+//                 });
+//             } 
+//         });
+//     }
+// });
+
+// chrome.tabs.onRemoved.addListener(function(tabId, removeInfo){
+//     chrome.storage.sync.get(['tabID'], function(data){
+//         alert(tabId + "  -  " + data.tabID + "  -  0  -  " + Date.now());
+//     });
+// });
+
