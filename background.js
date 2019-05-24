f208dc09ca7ff6cabc12bb2db2665f6a6142c343
Paunczki <johnnymikos@gmail.com>
2019-05-24 13:24:43 -0500

    fixed tab issue

--- background.js
+++ background.js
@@ -1,18 +1,36 @@
+var tabIdToPreviousUrl = {};
+
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
-    if(changeInfo.status === 'loading') {
-        var bs = tab.url;
-        chrome.storage.sync.get(['tabID'], function(data){
-            if(!(data.tabID === bs)){
-                alert(tabId + "  -  " + data.tabID + "  -  0  -  " + Date.now());
-                chrome.storage.sync.set({'tabID': bs}, function(){
-                    alert(tabId + "  -  " + bs + "  -  1  -  " + Date.now());
-                });
-            } 
-        });
+    if(changeInfo.status === 'loading'){
+        var previousUrl = "";
+        previousUrl = tabIdToPreviousUrl[tabId];
+        if (previousUrl !== changeInfo.url) {
+            alert(tabId + "  -  " + previousUrl + "  -  0  -  " + Date.now());
+            alert(tabId + "  -  " + changeInfo.url + "  -  1  -  " + Date.now());
+        }
+        tabIdToPreviousUrl[tabId] = changeInfo.url;
     }
 });
-chrome.tabs.onRemoved.addListener(function(tabId, removeInfo){
-    chrome.storage.sync.get(['tabID'], function(data){
-        alert(tabId + "  -  " + data.tabID + "  -  0  -  " + Date.now());
-    });
-});
+
+// https://stackoverflow.com/questions/3809401/what-is-a-good-regular-expression-to-match-a-url
+
+// chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
+//     if(changeInfo.status === 'loading') {
+//         id = tabId;
+//         bs = tab.url;
+//         chrome.storage.sync.get(['id'], function(data){
+//             //need to find a way to make sites tabs unique
+//             if(!(data.id === bs)){
+//                 alert(tabId + "  -  " + data.id + "  -  0  -  " + Date.now());
+//                 chrome.storage.sync.set({id: bs}, function(){
+//                     alert(tabId + "  -  " + bs + "  -  1  -  " + Date.now());
+//                 });
+//             }
+//         });
+//     }
+// });
+// chrome.tabs.onRemoved.addListener(function(tabId, removeInfo){
+//     chrome.storage.sync.get(['id'], function(data){
+//         alert(tabId + "  -  " + data.id + "  -  0  -  " + Date.now());
+//     });
+// });
