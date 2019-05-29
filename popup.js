1c35d23f83d4c7e1f65d9e726a557c1265be0e07
Laura Adams <laura.adams2233@gmail.com>
2019-05-29 10:01:28 -0500

    Working On/Off switch with onUpdated/onRemoved

--- popup.js
+++ popup.js
@@ -1,59 +1,23 @@
-// var port = chrome.extension.connect({
-//     name: "Sample Communication"
-// });
-// port.postMessage("Hi BackGround");
-// port.onMessage.addListener(function(msg) {
-//     console.log("message recieved" + msg);
-// });
+$(function() {
+    $("#togBtn").on('change', function(e) {
+        if (e.target.checked){
+            chrome.storage.local.set({'sS': true}, function(){
+                alert('saved: True');
+            });
+        } else {
+            chrome.storage.local.set({'sS': false}, function(){
+                alert('saved: False');
+            });
+        }   
+    });
 
-//the call is working because the popup alerts when switch status has changed. however, it is not saving across all tabs. 
-$(function() { 
-    chrome.storage.local.get('switchStatus', function(status){
-        switchStatus = status.switchStatus;
-        $("#togBtn").on('change', function() {
-            if($('#box').is(':checked')) {
-                switchStatus = $("#togBtn").is(':checked');
-                alert(switchStatus); //to verify
- 
-                chrome.storage.local.set({swtichStatus: true}, function(){
-                    alert('saved' + ' : ' + switchStatus);
-                });
+    chrome.storage.local.get('sS', function(status){
+        var switchStatus = status.sS;
 
-          
-            }
-            else {
-                switchStatus = $("#togBtn").is(':checked');
-                alert(switchStatus); //to verify
-
-                chrome.storage.local.set({switchStatus: false}, function(){
-                    alert('saved' + ' : ' + switchStatus);
-                });
-            }
-        });
-    })
+        if(switchStatus) {
+            $('#togBtn').prop('checked', true);
+        } else {
+            $('#togBtn').prop('checked', false);
+        }
+    });
 });
-
-
-
-// $(function() {
-//     var switchStatus = false;
-//     $("#togBtn").on('change', function() {
-//         if($('#box').is(':checked')) {
-//             switchStatus = $("#togBtn").is(':checked');
-//             alert(switchStatus); //to verify
-//             chrome.storage.local.set({'m': switchStatus}, function(){
-//                 alert('saved' + ' : ' + switchStatus);
-//             });
-          
-//         }
-//         else {
-//             switchStatus = $("#togBtn").is(':checked');
-//             alert(switchStatus); //to verify
-//             chrome.storage.local.set({'m': switchStatus}, function(){
-//                 alert('saved' + ' : ' + switchStatus);
-//             });
-//         }
-//     });
-// });
-
-
