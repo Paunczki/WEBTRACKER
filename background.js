e668e790f407c775907ac0d7ce8e4edf2bcc0208
paunczki <johnnymikos@gmail.com>
2019-06-14 14:20:36 -0500

    Records data based off time input (Default 20s)

--- background.js
+++ background.js
@@ -1,4 +1,6 @@
 var tabIdToPreviousUrl = {};
+var nowTime = Date.now();
+var timeIncremement = 20000;
 
 // function writeToDB(input){
 //     $.ajax({
@@ -16,49 +18,66 @@ chrome.storage.local.get('sS', function(status){
     switchStatus = status.sS;
 });
 
+chrome.storage.local.get('time', function(stamp){
+    timeIncrement = stamp.time;
+})
+
 chrome.storage.onChanged.addListener(function() {
     chrome.storage.local.get('sS', function(status){
         switchStatus = status.sS;
+        if(switchStatus === true){
+            nowTime = Date.now();
+        }
     });
 });
 
-var newWebsite;
+var newWebsite; var timers; var previousWebsite;
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
-    if(switchStatus === true){
-        if(changeInfo.status === 'complete'){
+    timers = nowTime + timeIncremement;
+    if((switchStatus === true)&&(Date.now() < timers)){
+        if((!(newWebsite === previousWebsite))&&(!(newWebsite === 'newtab'))&&(changeInfo.status === 'complete')){
             var sendDone = tabId + "," + newWebsite + ",2," + Date.now();
             alert(sendDone);
             //writeToDB(sendStart);
         }
-        var previousUrl = "";
-        previousUrl = tabIdToPreviousUrl[tabId];
-        var preU = ''; var newU = '';
-        if(previousUrl === undefined){
-            preU = 'undefined';
+        if(changeInfo.status === 'loading'){
+            var previousUrl = "";
+            previousUrl = tabIdToPreviousUrl[tabId];
+            var preU = ''; var newU = '';
+            if(previousUrl === undefined){
+                preU = 'undefined';
+            }
+            if(!(previousUrl === undefined)){
+                var array1 = previousUrl.split('/');
+                preU = array1[2];
+                previousWebsite = preU;
+            }
+            var array2 = (changeInfo.url).split('/');
+            newU = array2[2];
+            newWebsite = newU;
+            if((preU !== newU)&&(!(preU==='undefined'))&&(!(preU==='newtab'))&&(!(newU==='newtab'))){
+                var sendEnd = tabId + "," + preU + ",0," + Date.now();
+                alert(sendEnd);
+                //writeToDB(sendEnd);
+            }
+            if((preU !== newU)&&(!(newU==='newtab'))&&(changeInfo.status === 'loading')){
+                var sendStart = tabId + "," + newU + ",1," + Date.now();
+                alert(sendStart);
+                //writeToDB(sendStart);
+            }
+            tabIdToPreviousUrl[tabId] = changeInfo.url;
         }
-        if(!(previousUrl === undefined)){
-            var array1 = previousUrl.split('/');
-            preU = array1[2];
-        }
-        var array2 = (changeInfo.url).split('/');
-        newU = array2[2];
-        newWebsite = newU;
-        if((preU !== newU)&&(!(preU==='undefined'))&&(!(preU==='newtab'))&&(!(newU==='newtab'))){
-            var sendEnd = tabId + "," + preU + ",0," + Date.now();
-            alert(sendEnd);
-            //writeToDB(sendEnd);
-        }
-        if((preU !== newU)&&(!(newU==='newtab'))&&(changeInfo.status === 'loading')){
-            var sendStart = tabId + "," + newU + ",1," + Date.now();
-            alert(sendStart);
-            //writeToDB(sendStart);
-        }
-        tabIdToPreviousUrl[tabId] = changeInfo.url;
+    }
+    if((switchStatus === true)&&(Date.now() > timers)){
+        chrome.storage.local.set({'sS': false}, function(){
+            switchStatus = false;
+        });
     }
 });
 
 chrome.tabs.onRemoved.addListener(function(tabId, removeInfo){
-    if(switchStatus === true){
+    timers = nowTime + timeIncrement
+    if((switchStatus === true)&&(Date.now() < timers)){
         var previousUrl = "";
         previousUrl = tabIdToPreviousUrl[tabId];
         var preU = ''; 
--- options.js
+++ options.js
@@ -1,8 +1,13 @@
 $(function(){
     $('#saveTime').click(function(){
-        var time = $('#time').val();
-        if(time){
+        var time = Number($('#timer').val());
+        if(Number.isNaN(time)){
+            alert('Please use actual numbers');
+        }
+        else{
             chrome.storage.sync.set({'time' : time}, function(){
+                alert('Value saved as --> ' + time + ' milliseconds');
+                alert('To begin recording open the popup and move the switch to "ON"');
                 close();
             });
         }
