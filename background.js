a8077dcc204aa11c29ad87e5574514f2df02a157
paunczki <johnnymikos@gmail.com>
2019-06-14 09:42:12 -0500

    Found error where status 'complete' does not hold

--- background.js
+++ background.js
@@ -22,13 +22,8 @@ chrome.storage.onChanged.addListener(function() {
 var statusW = '';
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
     if(switchStatus === true){
-        if(changeInfo.status === 'loading'){
-            statusW = '1';
-        }
-        if(changeInfo.status === 'complete'){
-            statusW = '2';
-        }
-        //if(changeInfo.status === 'loading'){
+        // alert(changeInfo.status);
+        // if(changeInfo.status === 'loading'){
             var previousUrl = "";
             previousUrl = tabIdToPreviousUrl[tabId];
             var preU = ''; var newU = '';
@@ -40,19 +35,29 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
                 preU = array1[2];
             }
             var array2 = (changeInfo.url).split('/');
+            /*
+                ISSUE!!!!
+                Right here changeInfo.status forgets the status 'complete'
+                Need to research this so that we can have our on complete work also
+            */
             newU = array2[2];
             if((preU !== newU)&&(!(preU==='undefined'))&&(!(preU==='newtab'))&&(!(newU==='newtab'))){
                 var sendEnd = tabId + "," + preU + ",0," + Date.now();
                 alert(sendEnd);
                 //writeToDB(sendEnd);
             }
-            if((preU !== newU)&&(!(newU==='newtab'))){
-                var sendStart = tabId + "," + newU + "," + statusW + "," + Date.now();
+            if((preU !== newU)&&(!(newU==='newtab'))&&(changeInfo.status === 'loading')){
+                var sendStart = tabId + "," + newU + ",1," + Date.now();
                 alert(sendStart);
                 //writeToDB(sendStart);
             }
+            if((!(newU==='newtab'))&&(changeInfo.status === 'complete')){
+                var sendDone = tabId + "," + newU + ",2," + Date.now();
+                alert(sendDone);
+                //writeToDB(sendStart);
+            }
             tabIdToPreviousUrl[tabId] = changeInfo.url;
-        //}
+        // }
     }
 });
 
