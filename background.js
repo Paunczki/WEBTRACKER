be917a4fc7f4148b78b5ed746263b0a1ba8663ae
Paunczki <johnnymikos@gmail.com>
2019-07-09 09:26:53 -0500

    Found many methods to attempt to connect to server

--- background.js
+++ background.js
@@ -2,6 +2,8 @@ var tabIdToPreviousUrl = {};
 var nowTime = Date.now();
 var timeIncremement = 20000;
 
+//
+
 function sendInfo(input){
     var webSocket = new WebSocket("wss://localhost:8123");
     webSocket.onopen = function() {
@@ -10,7 +12,42 @@ function sendInfo(input){
     webSocket.close();
 }
 
-// function writeToDB(input){
+/*
+function sendInfo(input){
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "localhost:8123", true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify({
+        value: value
+    }));
+    xhr.close();
+}
+*/
+
+/*
+function sendInfo(input) {
+    function posAjax(url,data, success) {
+        var params = typeof data == 'string' ? data : Object.keys(data).map(
+            function(k){
+                return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])
+            }
+        ).join('&');
+        var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
+        xhr.open('POST', url);
+        xhr.onreadystatechange = function() {
+            if(xhr.readyState>3 && xhr.status==200){
+                success(xhr.responseText);
+            }
+        };
+        xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+        chr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        chr.send(params);
+        return xhr;
+    }
+}
+*/
+
+// function sendInfo(input){
 //     $.ajax({
 //         type: 'post',
 //         url: 'https://localhost/db1/script.php',
