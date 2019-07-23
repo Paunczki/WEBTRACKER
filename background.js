21a5b4eb95d047655ab0890c97d33955f0a4398f
Paunczki <johnnymikos@gmail.com>
2019-07-23 09:31:23 -0500

    Fixed issue with closing, and hopefully onLoaded

--- background.js
+++ background.js
@@ -1,8 +1,6 @@
 var tabIdToPreviousUrl = {};
 var nowTime = Date.now();
-var timeIncremement = 20000;
-
-// localhost:8123
+var timeIncremement = 5000;
 
 function sendInfo(input){
     // alert("hello " + input);
@@ -15,62 +13,6 @@ function sendInfo(input){
     //   request.send(input);
     // }
 }
-
-/*
-function sendInfo(input){
-    var webSocket = new WebSocket("wss://127.0.0.1:8123");
-    webSocket.onopen = function() {
-        webSocket.send(input);
-    }
-    webSocket.close();
-}
-*/
-
-/*
-function sendInfo(input){
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "localhost:8123", true);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send(JSON.stringify({
-        input: input
-    }));
-    xhr.close();
-}
-*/
-
-/*
-function sendInfo(input) {
-    function posAjax("localhost:8123",input, success) {
-        var params = typeof data == 'string' ? input : Object.keys(data).map(
-            function(k){
-                return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])
-            }
-        ).join('&');
-        var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
-        xhr.open('POST', 'localhost:8123');
-        xhr.onreadystatechange = function() {
-            if(xhr.readyState>3 && xhr.status==200){
-                success(xhr.responseText);
-            }
-        };
-        xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-        chr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-        chr.send(params);
-        return xhr;
-    }
-}
-*/
-
-// function sendInfo(input){
-//     $.ajax({
-//         type: 'post',
-//         url: 'https://localhost/db1/script.php',
-//         data: {input: input},
-//         success: function(){
-//             alert("it workin'");
-//         }
-//     })
-// }
 
 var switchStatus = false;
 chrome.storage.local.get('sS', function(status){
@@ -96,8 +38,11 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
     if((switchStatus === true)&&(Date.now() < timers)){
         if((!(newWebsite === previousWebsite))&&(!(newWebsite === 'newtab'))&&(changeInfo.status === 'complete')){
             var sendDone = tabId + "," + newWebsite + ",2," + Date.now();
-            alert(sendDone);
-            sendInfo(sendStart);
+            chrome.storage.local.set({'sendDone':sendDone}, function(){});
+            chrome.storage.local.get('sendDone', function(status){
+                alert(status.sendDone);
+                // sendInfo(sendStart);
+            });
         }
         if(changeInfo.status === 'loading'){
             var previousUrl = "";
@@ -117,12 +62,12 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
             if((preU !== newU)&&(!(preU==='undefined'))&&(!(preU==='newtab'))&&(!(newU==='newtab'))){
                 var sendEnd = tabId + "," + preU + ",0," + Date.now();
                 alert(sendEnd);
-                sendInfo(sendEnd);
+                // sendInfo(sendEnd);
             }
             if((preU !== newU)&&(!(newU==='newtab'))&&(changeInfo.status === 'loading')){
                 var sendStart = tabId + "," + newU + ",1," + Date.now();
                 alert(sendStart);
-                sendInfo(sendStart);
+                // sendInfo(sendStart);
             }
             tabIdToPreviousUrl[tabId] = changeInfo.url;
         }
@@ -134,9 +79,10 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
     }
 });
 
+var timers2;
 chrome.tabs.onRemoved.addListener(function(tabId, removeInfo){
-    timers = nowTime + timeIncrement
-    if((switchStatus === true)&&(Date.now() < timers)){
+    timers2 = nowTime + timeIncremement;
+    if((switchStatus === true)&&(Date.now() < timers2)){
         var previousUrl = "";
         previousUrl = tabIdToPreviousUrl[tabId];
         var preU = ''; 
@@ -150,7 +96,12 @@ chrome.tabs.onRemoved.addListener(function(tabId, removeInfo){
         if(!(preU === 'newtab')&&(!(preU === undefined))){
             var sendClosed = tabId + "," + preU + ",0," + Date.now();
             alert(sendClosed);
-            sendInfo(sendClosed);
+            // sendInfo(sendClosed);
         }
+    }
+    if((switchStatus === true)&&(Date.now() > timers2)){
+        chrome.storage.local.set({'sS': false}, function(){
+            switchStatus = false;
+        });
     }
 });
