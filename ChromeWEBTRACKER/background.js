ba68ff9b1b7ab8142d3531c2fb6ff81aab40dc8c
Paunczki <johnnymikos@gmail.com>
2019-10-02 13:56:53 -0500

    Removed timer for these versions

--- ChromeWEBTRACKER/background.js
+++ ChromeWEBTRACKER/background.js
@@ -24,113 +24,64 @@ chrome.storage.local.get('userid', function(items) {
 });
 
 var tabIdToPreviousUrl = {};
-var nowTime = Date.now();
-var timeIncremement = 20000;
 
 function sendInfo(input){
-    // alert("hello " + input);
     var request = new XMLHttpRequest();
-    var url = "https://cybersecurity.cs.luc.edu/JohnTest/" + input;
+    var url = "https://cybersecurity.cs.luc.edu/WebTracker/" + input;
     request.open("POST", url, true); 
     request.setRequestHeader("Content-Type", "text/plain;charset=UTF-8"); 
     request.send("data")
-    // request.onreadystatechange = function () {
-    //   request.send(input);
-    // }
 }
 
-var switchStatus = false;
-chrome.storage.local.get('sS', function(status){
-    switchStatus = status.sS;
-});
-
-chrome.storage.local.get('time', function(stamp){
-    timeIncrement = stamp.time;
-})
-
-chrome.storage.onChanged.addListener(function() {
-    chrome.storage.local.get('sS', function(status){
-        switchStatus = status.sS;
-        if(switchStatus === true){
-            nowTime = Date.now();
-        }
-    });
-});
-
-var newWebsite; var timers; var previousWebsite;
+var newWebsite; var previousWebsite;
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
-    timers = nowTime + timeIncremement;
-    if((switchStatus === true)&&(Date.now() < timers)){
-        if((!(newWebsite === previousWebsite))&&(!(newWebsite === 'newtab'))&&(changeInfo.status === 'complete')){
-            // var sendDone = userid + "," + tabId + "," + newWebsite + ",2," + Date.now();
-            var sendDone = Date.now() + ":" + userid + ":" + tabId + ":" + newWebsite + ":2";
-            chrome.storage.local.set({'sendDone':sendDone}, function(){});
-            chrome.storage.local.get('sendDone', function(status){
-                // alert(status.sendDone);
-                sendInfo(status.sendDone);
-            });
-        }
-        if(changeInfo.status === 'loading'){
-            var previousUrl = "";
-            previousUrl = tabIdToPreviousUrl[tabId];
-            var preU = ''; var newU = '';
-            if(previousUrl === undefined){
-                preU = 'undefined';
-            }
-            if(!(previousUrl === undefined)){
-                var array1 = previousUrl.split('/');
-                preU = array1[2];
-                previousWebsite = preU;
-            }
-            var array2 = (changeInfo.url).split('/');
-            newU = array2[2];
-            newWebsite = newU;
-            if((preU !== newU)&&(!(preU==='undefined'))&&(!(preU==='newtab'))&&(!(newU==='newtab'))){
-                // var sendEnd = userid + "," + tabId + "," + preU + ",0," + Date.now();
-                var sendEnd = Date.now() + ":" + userid + ":" + tabId + ":" + preU + ":0";
-                // alert(sendEnd);
-                sendInfo(sendEnd);
-            }
-            if((preU !== newU)&&(!(newU==='newtab'))&&(changeInfo.status === 'loading')){
-                // var sendStart = userid + "," + tabId + "," + newU + ",1," + Date.now();
-                var sendStart = Date.now() + ":" + userid + ":" + tabId + ":" + newU + ":1";
-                // alert(sendStart);
-                sendInfo(sendStart);
-            }
-            tabIdToPreviousUrl[tabId] = changeInfo.url;
-        }
-    }
-    if((switchStatus === true)&&(Date.now() > timers)){
-        chrome.storage.local.set({'sS': false}, function(){
-            switchStatus = false;
+    if((!(newWebsite === previousWebsite))&&(!(newWebsite === 'newtab'))&&(changeInfo.status === 'complete')){
+        var sendDone = Date.now() + ":" + userid + ":" + tabId + ":" + newWebsite + ":2";
+        chrome.storage.local.set({'sendDone':sendDone}, function(){});
+        chrome.storage.local.get('sendDone', function(status){
+            sendInfo(status.sendDone);
         });
     }
-});
-
-var timers2;
-chrome.tabs.onRemoved.addListener(function(tabId, removeInfo){
-    timers2 = nowTime + timeIncremement;
-    if((switchStatus === true)&&(Date.now() < timers2)){
+    if(changeInfo.status === 'loading'){
         var previousUrl = "";
         previousUrl = tabIdToPreviousUrl[tabId];
-        var preU = ''; 
+        var preU = ''; var newU = '';
         if(previousUrl === undefined){
             preU = 'undefined';
         }
         if(!(previousUrl === undefined)){
             var array1 = previousUrl.split('/');
             preU = array1[2];
+            previousWebsite = preU;
         }
-        if(!(preU === 'newtab')&&(!(preU === undefined))){
-            // var sendClosed = userid + "," + tabId + "," + preU + ",0," + Date.now();
-            var sendClosed = Date.now() + ":" + userid + ":" + tabId + ":" + preU + ":0";
-            // alert(sendClosed);
-            sendInfo(sendClosed);
+        var array2 = (changeInfo.url).split('/');
+        newU = array2[2];
+        newWebsite = newU;
+        if((preU !== newU)&&(!(preU==='undefined'))&&(!(preU==='newtab'))&&(!(newU==='newtab'))){
+            var sendEnd = Date.now() + ":" + userid + ":" + tabId + ":" + preU + ":0";
+            sendInfo(sendEnd);
         }
+        if((preU !== newU)&&(!(newU==='newtab'))&&(changeInfo.status === 'loading')){
+            var sendStart = Date.now() + ":" + userid + ":" + tabId + ":" + newU + ":1";
+            sendInfo(sendStart);
+        }
+        tabIdToPreviousUrl[tabId] = changeInfo.url;
     }
-    if((switchStatus === true)&&(Date.now() > timers2)){
-        chrome.storage.local.set({'sS': false}, function(){
-            switchStatus = false;
-        });
+});
+
+chrome.tabs.onRemoved.addListener(function(tabId, removeInfo){
+    var previousUrl = "";
+    previousUrl = tabIdToPreviousUrl[tabId];
+    var preU = ''; 
+    if(previousUrl === undefined){
+        preU = 'undefined';
+    }
+    if(!(previousUrl === undefined)){
+        var array1 = previousUrl.split('/');
+        preU = array1[2];
+    }
+    if(!(preU === 'newtab')&&(!(preU === undefined))){
+        var sendClosed = Date.now() + ":" + userid + ":" + tabId + ":" + preU + ":0";
+        sendInfo(sendClosed);
     }
 });
--- MozillaWEBTRACKER/background.js
+++ MozillaWEBTRACKER/background.js
@@ -24,115 +24,68 @@ browser.storage.local.get('userid', function(items) {
 });
 
 var tabIdToPreviousUrl = {};
-var nowTime = Date.now();
-var timeIncremement = 10000;
 
 function sendInfo(input){
-    // alert("hello " + input);
     var request = new XMLHttpRequest();
-    var url = "https://cybersecurity.cs.luc.edu/JohnTest/" + input;
+    var url = "https://cybersecurity.cs.luc.edu/WebTracker/" + input;
     request.open("POST", url, true); 
     request.setRequestHeader("Content-Type", "text/plain;charset=UTF-8"); 
     request.send("data");
-    // request.onreadystatechange = function () {
-    //   request.send(input);
-    // }
 }
 
-var switchStatus = false;
-browser.storage.local.get('sS', function(status){
-    switchStatus = status.sS;
-});
-
-browser.storage.local.get('time', function(stamp){
-    timeIncrement = stamp.time;
-})
-
-browser.storage.onChanged.addListener(function() {
-    browser.storage.local.get('sS', function(status){
-        switchStatus = status.sS;
-        if(switchStatus === true){
-            nowTime = Date.now();
-        }
-    });
-});
-
-var newWebsite; var timers; var previousWebsite;
+var newWebsite; var previousWebsite;
 browser.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
-    timers = nowTime + timeIncremement;
-    if((switchStatus === true)&&(Date.now() < timers)){
-        if((!(newWebsite === previousWebsite))&&(!(newWebsite === 'newtab'))&&(changeInfo.status === 'complete')){
-            // var sendDone = userid + "," + tabId + "," + newWebsite + ",2," + Date.now();
-            var sendDone = Date.now() + ":" + userid + ":" + tabId + ":" + newWebsite + ":2";
-            browser.storage.local.set({'sendDone':sendDone}, function(){});
-            browser.storage.local.get('sendDone', function(status){
-                console.log(status.sendDone);
-                sendInfo(status.sendDone);
-            });
-        }
-        if(changeInfo.status === 'loading'){
-            var previousUrl = "";
-            previousUrl = tabIdToPreviousUrl[tabId];
-            var preU = ''; var newU = '';
-            if(previousUrl === undefined){
-                preU = 'undefined';
-            }
-            if(!(previousUrl === undefined)){
-                var array1 = previousUrl.split('/');
-                preU = array1[2];
-                previousWebsite = preU;
-            }
-            var array2 = (changeInfo.url).split('/');
-            newU = array2[2];
-            newWebsite = newU;
-            if((preU !== newU)&&(!(preU==='undefined'))&&(!(preU==='newtab'))&&(!(newU==='newtab'))){
-                // var sendEnd = userid + "," + tabId + "," + preU + ",0," + Date.now();
-                var sendEnd = Date.now() + ":" + userid + ":" + tabId + ":" + preU + ":0";
-                console.log(sendEnd);
-                sendInfo(sendEnd);
-            }
-            if((preU !== newU)&&(!(newU==='newtab'))&&(changeInfo.status === 'loading')){
-                // var sendStart = userid + "," + tabId + "," + newU + ",1," + Date.now();
-                var sendStart = Date.now() + ":" + userid + ":" + tabId + ":" + newU + ":1";
-                console.log(sendStart);
-                sendInfo(sendStart);
-            }
-            tabIdToPreviousUrl[tabId] = changeInfo.url;
-        }
-    }
-    if((switchStatus === true)&&(Date.now() > timers)){
-        console.log(1);
-        browser.storage.local.set({'sS': false}, function(){
-            switchStatus = false;
+    if((!(newWebsite === previousWebsite))&&(!(newWebsite === 'newtab'))&&(changeInfo.status === 'complete')){
+        var sendDone = Date.now() + ":" + userid + ":" + tabId + ":" + newWebsite + ":2";
+        browser.storage.local.set({'sendDone':sendDone}, function(){});
+        browser.storage.local.get('sendDone', function(status){
+            console.log(status.sendDone);
+            sendInfo(status.sendDone);
         });
     }
-});
-
-var timers2;
-browser.tabs.onRemoved.addListener(function(tabId, removeInfo){
-    timers2 = nowTime + timeIncremement;
-    if((switchStatus === true)&&(Date.now() < timers2)){
+    if(changeInfo.status === 'loading'){
         var previousUrl = "";
         previousUrl = tabIdToPreviousUrl[tabId];
-        var preU = ''; 
+        var preU = ''; var newU = '';
         if(previousUrl === undefined){
             preU = 'undefined';
         }
         if(!(previousUrl === undefined)){
             var array1 = previousUrl.split('/');
             preU = array1[2];
+            previousWebsite = preU;
         }
-        if(!(preU === 'newtab')&&(!(preU === undefined))){
-            // var sendClosed = userid + "," + tabId + "," + preU + ",0," + Date.now();
-            var sendClosed = Date.now() + ":" + userid + ":" + tabId + ":" + preU + ":0";
-            console.log(sendClosed);
-            sendInfo(sendClosed);
+        var array2 = (changeInfo.url).split('/');
+        newU = array2[2];
+        newWebsite = newU;
+        if((preU !== newU)&&(!(preU==='undefined'))&&(!(preU==='newtab'))&&(!(newU==='newtab'))){
+            var sendEnd = Date.now() + ":" + userid + ":" + tabId + ":" + preU + ":0";
+            console.log(sendEnd);
+            sendInfo(sendEnd);
         }
+        if((preU !== newU)&&(!(newU==='newtab'))&&(changeInfo.status === 'loading')){
+            var sendStart = Date.now() + ":" + userid + ":" + tabId + ":" + newU + ":1";
+            console.log(sendStart);
+            sendInfo(sendStart);
+        }
+        tabIdToPreviousUrl[tabId] = changeInfo.url;
     }
-    if((switchStatus === true)&&(Date.now() > timers2)){
-        console.log(2);
-        browser.storage.local.set({'sS': false}, function(){
-            switchStatus = false;
-        });
+});
+
+browser.tabs.onRemoved.addListener(function(tabId, removeInfo){
+    var previousUrl = "";
+    previousUrl = tabIdToPreviousUrl[tabId];
+    var preU = ''; 
+    if(previousUrl === undefined){
+        preU = 'undefined';
+    }
+    if(!(previousUrl === undefined)){
+        var array1 = previousUrl.split('/');
+        preU = array1[2];
+    }
+    if(!(preU === 'newtab')&&(!(preU === undefined))){
+        var sendClosed = Date.now() + ":" + userid + ":" + tabId + ":" + preU + ":0";
+        console.log(sendClosed);
+        sendInfo(sendClosed);
     }
 });
