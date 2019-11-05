e346035faf7145bdd324da29f5199d55022183bb
Paunczki <johnnymikos@gmail.com>
2019-11-04 18:26:07 -0600

    Working on server information

---
+++ Server/parse.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class parse{
+    static String filepath = "/Logs/log";
+    static File log = new File(filepath);
+    public static void main(String[] args){
+        System.out.println(log);
+    }
+}
