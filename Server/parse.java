aaf881e0702aafee6ec65f312a5a495a8b01c128
Paunczki <johnnymikos@gmail.com>
2019-11-06 17:50:07 -0600

    Worked on parsing data

--- Server/parse.java
+++ Server/parse.java
@@ -1,9 +1,22 @@
 import java.io.*;
+import java.util.*;
 
 public class parse{
-    static String filepath = "/Logs/log";
+    static String filepath = "Server/Logs/log_test.txt";
     static File log = new File(filepath);
-    public static void main(String[] args){
+    public static void main(String[] args) throws FileNotFoundException{
+        Scanner scanner = new Scanner(log);
         System.out.println(log);
+        while(scanner.hasNextLine()){
+            String line = scanner.nextLine();
+            String[] b = line.split(" ");
+            if(b[4].equals("\"POST")){
+                // b[5] is the information
+                String[] c = b[5].split("/");
+                System.out.println(c[2]);
+                // c[2] is the information we want
+            }
+        }
+        scanner.close();
     }
 }
